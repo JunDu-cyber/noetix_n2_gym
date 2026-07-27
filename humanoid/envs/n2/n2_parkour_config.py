3a43da8e00f3e80c09455a0f1d1e73c9f2b0ab60
JunDu <dujun173@gmail.com>
2026-07-27 16:26:26 +0800

    chore(param)

--- humanoid/envs/n2/n2_parkour_config.py
+++ humanoid/envs/n2/n2_parkour_config.py
@@ -141,7 +141,7 @@ class N2ParkourCfg(N2PerceptiveCfg):
 
         class scales(N2PerceptiveCfg.rewards.scales):
 
-            tracking_goal_vel = 3.5
+            tracking_goal_vel = 3.2
             tracking_yaw = 0.5          # EP: tracking_yaw = 0.5
             goal_reached = 0.0          # 非 EP 项，默认关闭
 
